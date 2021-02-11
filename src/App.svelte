7802fbb56e22f7c92b62a6e75439b2e48300db35
Matt Barnfield <matt.barnfield@softwire.com>
2021-02-11 13:13:41 +0000

    Add svelte and scss

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Header from './Header.svelte'
+</script>
+
+<style type="text/scss" global>
+	@import './colors.scss';
+
+	:global(body) {
+		background-color: $light-background;
+		color: $dark-text;
+		padding: 0;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+	}
+
+	h1 {
+		text-transform: uppercase;
+		font-size: 3em;
+		font-weight: 100;
+	}
+</style>
+
+<Header />
+<main>
+	<h1>A Mechanical Awakening</h1>
+	<p>
+		Welcome to the official site of <em>A Mechanical Awakening</em>!
+		Here you'll be able to find all sorts of resources about your adventures in Eberron!
+	</p>
+</main>
+
---
+++ src/Header.svelte
@@ -0,0 +1,23 @@
+<style type="text/scss">
+    @import 'colors.scss';
+
+    header {
+      background-color: $dark-background;
+      color: $light-text;
+      padding: 20px;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+
+        &.home-link {
+          font-size: 2em;
+          font-weight: 100;
+        }
+      }
+    }
+</style>
+
+<header>
+    <a class="home-link" href="/">A Mechanical Awakening</a>
+</header>
